6b2db208a83f96d92322d124e366252cc9621048
OLC <utsumabo@ritsukage.com>
2026-04-08 02:24:16 +0800

    chore: Bump version to 0.3.3 in Const.cs and mod_manifest.json
    - Updated version number to 0.3.3 in both Const.cs and mod_manifest.json to reflect the latest changes.

--- Const.cs
+++ Const.cs
@@ -3,7 +3,7 @@
     public static class Const
     {
         public const string ModId = "com.ritsukage.sts2-ShowPlayerHandCards";
-        public const string Version = "0.3.2";
+        public const string Version = "0.3.3";
         public const string SettingsFileName = "settings.json";
     }
 }
